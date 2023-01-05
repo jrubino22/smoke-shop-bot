d388a6a59c1f390e529a88866bba186b4ffb5157
Joseph Rubino <joeyrubi22@gmail.com>
2023-01-05 17:07:43 -0600

    csv creation

--- finder.js
+++ finder.js
@@ -1,12 +1,15 @@
 const playwright = require('playwright');
 const browserType = 'chromium';
+const { expect } = require('@playwright/test');
+const fs = require('fs');
+
+
 async function main() {
   const shopNameArray = [];
   const phoneNumArray = [];
   const addressArray = [];
-  const websiteArray = [];
 
-  for (let zipCode = 10001; zipCode < 10005; zipCode++) {
+  for (let zipCode = 10001; zipCode < 10002; zipCode++) {
     const browser = await playwright[browserType].launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
@@ -24,22 +27,53 @@ async function main() {
     console.log(' sc ', links);
     try {
       await links.click();
+      await page.waitForLoadState('load');
+      await page
+        .locator('div[role="feed"]')
+        .evaluate((e) => (e.scrollTop = e.scrollHeight * 2));
     } catch {
       zipCode = zipCode - 1;
     }
-    await page.waitForLoadState('load');
-    await page.waitForTimeout(3000);
 
-    const storeContainer = page.locator('div[data-js-log-root]');
-
+    await page.waitForTimeout(2000);
+    const storeContainer = page.locator('[role="article"]');
     const count = await storeContainer.count();
-    const storeName =
-      page.locator('span[class="OSrXXb"]') ||
-      page.locator('div[role="heading"]');
     console.log('count', count);
+    const re = new RegExp([0 - 9]);
+
     for (let i = 0; i < count; i++) {
-      console.log('text', await storeContainer.nth(i));
-      shopNameArray.push(await storeContainer.nth(i).textContent());
+      const c = storeContainer.nth(i).locator('[jstcache="154"]');
+      const n = await c.count();
+
+      const phoneNum = await storeContainer
+        .nth(i)
+        .locator('[jstcache="154"]')
+        .nth(n - 1)
+        .textContent();
+
+      console.log('phone: ', phoneNum);
+
+      try {
+        shopNameArray.push(
+          await storeContainer.nth(i).getAttribute('aria-label')
+        );
+        addressArray.push(
+          (await storeContainer
+            .nth(i)
+            .locator('[jstcache="154"]')
+            .nth(1)
+            .textContent()) +
+            ' - ' +
+            zipCode
+        );
+        if (phoneNum) {
+          phoneNumArray.push(phoneNum);
+        } else {
+          phoneNumArray.push('not listed');
+        }
+      } catch {
+        i = i;
+      }
     }
 
     await page.waitForTimeout(3000);
@@ -48,30 +82,39 @@ async function main() {
   }
 
   console.log('shop names', shopNameArray);
+  console.log('phone Numbers', phoneNumArray);
+  console.log('addresses', addressArray);
+
+  const headers = ['Name', 'Phone Number', 'Address'];
+  const columns = [shopNameArray, phoneNumArray, addressArray];
+
+  function csvRows(headers, columns) {
+    const output = [headers];
+    const numRows = columns
+      .map((col) => col.length)
+      .reduce((a, b) => Math.max(a, b));
+
+    for (let row = 0; row < numRows; row++) {
+      output.push(columns.map((c) => c[row] || ''));
+    }
+
+    return output;
+  }
+
+  function csvString(data) {
+    let output = '';
+    data.forEach((row) => (output += row.join(',') + '\n'));
+    return output;
+  }
+
+  function csvConstructor(headers, columns) {
+    return csvString(csvRows(headers, columns));
+  }
+
+
+fs.appendFile(`shops.csv`, csvConstructor(headers, columns), function (err) {
+  if (err) throw err;
+})
+
 }
 main();
-
-// console.log('sc', storeContainer);
-// storeContainer.evaluateAll(
-//   shopNameArray.push(
-//     await page.$('div[class="fontHeadlineSmall"]').textContent
-//   )
-// );
-
-// const count1 = await expect(page.$('a[data-hveid="CAEQAw"]')).toHaveCount(0)
-// const findFunction = () => {
-//   if (count1) {
-//     return page.locator('a[data-hveid="CAEQAw"]');
-//   } else if (page.$('a[data-hveid="CAMQAw"]')) {
-//     return page.locator('a[data-hveid="CAMQAw"]');
-//   } else if (page.$('a[data-hveid="CAEQAw"]')) {
-//     return page.locator('a[data-hveid="CAEQAw"]');
-//   } else {
-//     zipCode = zipCode - 1;
-//   }
-// };
-
-//  const mapClicks1 = page.$('a[data-hveid="CAEQAw"]');
-//   const mapClicks2 = page.$('a[data-hveid="CAMQAw"]');
-//   const mapClicks3 = page.$('a[data-hveid="CAIQAw"]');
-//  mapClicks1 ? await mapClicks1.click() : mapClicks2 ? await mapClicks2.click() : mapClicks3 ? await mapClicks3.click() : page.refresh()
